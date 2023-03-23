9dff87265588f06e959f8e6e773471c6f0a338d0
msg2324 <igiza1213@gmail.com>
2023-03-23 14:26:50 +0900

    어라

--- Hello/src/App.java
+++ Hello/src/App.java
@@ -1,15 +1,29 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int a[];
-        int[] b;
-        int[] c = {31, 32, 33};
-        a =new int[4];
-        b = new int[]{21, 22, 23, 24};
-        c = b;
-        System.out.println(a.length + " " + b.length + " " + c.length);
+        
+        // int a[];
+        // int[] b;
+        // int[] c = {31, 32, 33};
+        // a = new int[4];
+        // b = new int[]{21, 22, 23, 24};
+        // c = b;
+        // System.out.println(a.length + " " + b.length + " " + c.length);
 
+        int score[] = new int[5];
+        int sum = 0;
+        float avg;
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.print("성적입력: ");
+            score[i] = sc.nextInt();
+            sum += score[i];
+        }
+        System.out.println("합계 : " + sum);
+        avg = sum / 5;
+        System.out.println("평균 : " + avg);
+        sc.close();
     }
 }
 
@@ -35,20 +49,3 @@ public class App {
 
 
 
-
-
-
-// int score[] = new int[10];
-// int sum = 0;
-// float avg;
-
-// Scanner sc = new Scanner(System.in);
-// for (int i = 0; i < 5; i++) {
-//     System.out.print("성적입력: ");
-//     score[i] = sc.nextInt();
-//     sum += score[i];
-// }
-// System.out.println("합계 : " + sum);
-// avg = sum / 5;
-// System.out.println("평균 : " + avg);
-// sc.close();
