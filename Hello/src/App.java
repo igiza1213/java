2370edc24d4468b3a54682bbcacea48e5b539630
msg2324 <igiza1213@gmail.com>
2023-03-30 16:00:24 +0900

    Update App.java

--- Hello/src/App.java
+++ Hello/src/App.java
@@ -1,43 +1,50 @@
-
-
 // import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
-    // int a[];
-    // int[] b;
-    // int[] c = {31, 32, 33};
-    // a = new int[4];
-    // b = new int[]{21, 22, 23, 24};
-    // c = b;
-    // System.out.println(a.length + " " + b.length + " " + c.length);
 
-    // int score[] = new int[5];
-    // int sum = 0;
-    // float avg;
-    // Scanner sc = new Scanner(System.in);
-    // for (int i = 0; i < 5; i++) {
-    //   System.out.print("성적입력: ");
-    //   score[i] = sc.nextInt();
-    //   sum += score[i];
-    // }
-    // System.out.println("합계 : " + sum);
-    // avg = sum / 5;
-    // System.out.println("평균 : " + avg);
-    // sc.close();
 
-    // int a[][] = new int[2][3];
-    // a[0][0] = 1; a[0][1] = 2; a[0][2] = 3;
-    // a[1][0] = 10; a[1][1] = 20; a[1][2] = 30;
-    // for (int i = 0; i < a.length; i++) {
-    //     for (int j = 0; j < a[i].length; j++) {
-    //         System.out.print(a[i][j] + "\t");
-    //     }
-    // }
-    
-    
+    int sum = 0;
+    System.out.print("1-2+3-4+5-6+7-8+9-10=");
+    for (int i = 1; i <= 10; i++) {
+        if (i % 2 == 0) { 
+            sum += i;
+        } else{
+            sum -= i;
+        }
+    }
+    System.out.print(sum);
 
-    
+    for(int i = 1; i <= 5; i++){
+        for(int j = 1; j <= 5; j++){
+            System.out.print(j + "\t");
+        }
+    System.out.print("\n");
+    }
+
+    for(int i = 1; i <= 5; i++){
+        for(int j = 1; j <= i; j++){
+            System.out.print(j + "\t");
+        }
+        System.out.print("\n");
+    }
+
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= i; j++) {
+            System.out.print("*\t" );
+        }
+        System.out.print("\n");
+    }
+
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 3; j >= i; j--) {
+            System.out.print("*\t" );
+        }
+        System.out.print("\n");
+    }
+
+
     }
 }
 
